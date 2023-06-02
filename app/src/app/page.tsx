f864b2f10843aed6c3573f9865dfaac654349a36
wiktohh <wiktor.rzeznicki01@gmail.com>
2023-06-02 20:04:23 +0200

    divide for components

--- app/src/app/page.tsx
+++ app/src/app/page.tsx
@@ -3,6 +3,7 @@ import Link from "next/link";
 import Image from "next/image";
 import "./globals.css";
 import heroImage from "../assets/hero-image.jpg";
+import MainSection from "@/components/MainSection";
 
 const LandingPage = () => {
   return (
@@ -15,25 +16,7 @@ const LandingPage = () => {
           </button>
         </Link>
       </div>
-      <main className="flex w-screen">
-        <section className="w-1/2 flex flex-col text-center justify-center items-center gap-4">
-          <h2 className="text-6xl font-medium">
-            Twój najlepszy towarzysz w nauce i sprawdzianach!
-          </h2>
-          <p className="text-2xl">
-            Zanurz się w świat interaktywnej nauki i sprawdzianów z QuizApp -
-            Twoim niezawodnym przewodnikiem do sukcesu edukacyjnego!
-          </p>
-          <Link href="/register">
-            <button className="w-1/10 outline-none py-2 px-6 bg-[#66d1f2] cursor-pointer font-medium text-white text-xl">
-              Dołącz do nas
-            </button>
-          </Link>
-        </section>
-        <section className="w-1/2">
-          <Image src={heroImage} alt="students photo" />
-        </section>
-      </main>
+      <MainSection />
     </div>
   );
 };
---
+++ app/src/components/MainSection.tsx
@@ -0,0 +1,29 @@
+import Link from "next/link";
+import Image from "next/image";
+import heroImage from "../assets/hero-image.jpg";
+
+const MainSection = () => {
+  return (
+    <main className="flex w-screen">
+      <section className="w-1/2 flex flex-col text-center justify-center items-center gap-4">
+        <h2 className="text-6xl font-medium">
+          Twój najlepszy towarzysz w nauce i sprawdzianach!
+        </h2>
+        <p className="text-2xl">
+          Zanurz się w świat interaktywnej nauki i sprawdzianów z QuizApp -
+          Twoim niezawodnym przewodnikiem do sukcesu edukacyjnego!
+        </p>
+        <Link href="/register">
+          <button className="w-1/10 outline-none py-2 px-6 bg-[#66d1f2] cursor-pointer font-medium text-white text-xl">
+            Dołącz do nas
+          </button>
+        </Link>
+      </section>
+      <section className="w-1/2">
+        <Image src={heroImage} alt="students photo" />
+      </section>
+    </main>
+  );
+};
+
+export default MainSection;
