f0fcad98f6dba8efa7acea0b41a24217ea5e866e
wiktohh <wiktor.rzeznicki01@gmail.com>
2023-06-03 15:54:45 +0200

    fix h1 position

--- app/src/components/Header.tsx
+++ app/src/components/Header.tsx
@@ -2,7 +2,7 @@ import { BiUser } from "react-icons/bi";
 
 const Header = () => {
   return (
-    <header className="w-full h-[10%] p-8 bg-white flex justify-between">
+    <header className="w-full h-[10%] p-8 bg-white flex justify-between items-center">
       <h1 className="text-3xl font-medium">Quiz App</h1>
       <span className="flex text-xl gap-2 items-center justify-between">
         <BiUser />
