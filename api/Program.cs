8b60d8f1ec253dbc629a82cc6fc8d9b0fbb96dec
Jan Rapacz <jan.rapacz@gorrion.io>
2023-06-17 12:55:06 +0200

    fix cors

--- api/Program.cs
+++ api/Program.cs
@@ -30,7 +30,7 @@ builder.Services.AddCors(options =>
     options.AddPolicy(name: MyAllowSpecificOrigins,
                       policy  =>
                       {
-                          policy.WithOrigins(new string[]{"http://localhost:3000", "https://projekt-programowanie-2023.azurewebsites.net/api/"}).AllowAnyHeader().AllowAnyMethod();
+                          policy.WithOrigins(new string[]{"http://localhost:3000", "https://projekt-programowanie-2023.azurewebsites.net"}).AllowAnyHeader().AllowAnyMethod();
                       });
 });
 
