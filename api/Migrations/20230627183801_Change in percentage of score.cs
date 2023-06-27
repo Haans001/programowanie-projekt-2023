bcdde41fbd7840cf853192f99b1b63f9a6bac72b
Jakub Wajstak <jw303269@student.polsl.pl>
2023-06-27 20:39:33 +0200

    XD

---
+++ api/Migrations/20230627183801_Change in percentage of score.cs
@@ -0,0 +1,76 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace api.Migrations
+{
+    public partial class Changeinpercentageofscore : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Scores_Users_UserId",
+                table: "Scores");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Scores",
+                type: "integer",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AlterColumn<decimal>(
+                name: "PercentOfCorrectAnswers",
+                table: "Scores",
+                type: "numeric(5,2)",
+                precision: 5,
+                scale: 2,
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "integer");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Scores_Users_UserId",
+                table: "Scores",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Scores_Users_UserId",
+                table: "Scores");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserId",
+                table: "Scores",
+                type: "integer",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "integer",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "PercentOfCorrectAnswers",
+                table: "Scores",
+                type: "integer",
+                nullable: false,
+                oldClrType: typeof(decimal),
+                oldType: "numeric(5,2)",
+                oldPrecision: 5,
+                oldScale: 2);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Scores_Users_UserId",
+                table: "Scores",
+                column: "UserId",
+                principalTable: "Users",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
