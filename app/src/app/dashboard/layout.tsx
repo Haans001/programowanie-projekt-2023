e3360425459b94d96f1eae9b5473bd029cd5a5d6
wiktohh <wiktor.rzeznicki01@gmail.com>
2023-06-29 05:03:55 +0200

    remove sidebar

--- app/src/app/dashboard/layout.tsx
+++ app/src/app/dashboard/layout.tsx
@@ -10,8 +10,7 @@ const DashboardLayout = ({ children }: DashboardLayoutProps) => {
   return (
     <AuthProvider>
       <div className="flex">
-        <Navbar />
-        <div className="w-full ml-72">
+        <div className="w-full">
           <Header />
           <div className="w-full bg-gray-200 h-full p-6 min-h-[calc(100vh-64px)]">
             {children}
